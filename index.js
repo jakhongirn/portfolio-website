7e3c8965ed53712153246b979d600811723e455a
jaykhans <jahongirnagmatov@gmail.com>
2022-08-22 22:15:55 +0500

    install font inter-ui

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './src/App.jsx'
 import './src/index.css'
+import "inter-ui/inter.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
