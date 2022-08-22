af91c911af8b27dd1e8eac834f79d7dd74867c8e
jaykhans <jahongirnagmatov@gmail.com>
2022-08-22 21:47:17 +0500

    install and configure  tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Hey Universe! Happy hacking!</div>
+    <div className="text-3xl font-bold text-center">Hey Universe! Happy hacking!</div>
   )
 }
 
