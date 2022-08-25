c05766ad0ce748e06862a1a2b5a2371ad96e0568
jaykhans <jahongirnagmatov@gmail.com>
2022-08-25 16:20:05 +0500

    hero component update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="bg-lightcolor h-screen w-full grid ">
+      <div className="hero flex justify-center relative">
+        <div className="text-sm font-semibold rotate-90 absolute -right-4 bottom-20 uppercase">Scroll Down <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 inline"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg></div>
+        <div className="hero-circle "></div>
+        <div className="w-full px-64 absolute  grid grid-cols-2  justify-center items-end bottom-[13%]">
+          <div>
+            <h1 className="text-5xl font-semibold">
+              Hi,
+              <br />
+              I'm Jakhongir.
+              <br />
+              Software Engineer
+            </h1>
+            <h2 className="text-2xl mt-10">
+              Full Stack Developer / React.js Lover
+            </h2>
+
+            <button className="mt-10 text-lg font-semibold uppercase bg-[#E8E8E8] py-2 px-4 flex items-center gap-2 rounded-full">
+              See my works{' '}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 inline"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </button>
+
+            <div className="mt-10">
+              <a className="mr-6 underline" href="">Github</a> <span>/</span>
+              <a className="mx-6 underline" href="">Likedin</a> <span>/</span>
+              <a className="mx-6 underline" href="">Twiter</a>
+
+            </div>
+          </div>
+          <div>
+            <img src="images/ceaser.png" alt="" className="w-[30rem]" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+        <Hero />
+    </div>
   )
 }
 
