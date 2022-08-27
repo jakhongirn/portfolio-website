714742f3970dbe42e52ad6e885b4050eae8f03db
jaykhans <jahongirnagmatov@gmail.com>
2022-08-27 17:46:59 +0500

    format Navbar and Footer and little updates

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,29 +4,33 @@ import Button from '../components/cards/Button'
 const Footer = () => {
   return (
     <div className="bg-black text-white px-[15%] pt-[10%]">
-      <h1 className="text-5xl text-center font-bold">Let's work together.</h1>
-      <p className="text-center mt-4 text-graytext">I'm available for Consultancy & Freelancing.</p>
+      <h1 className="text-5xl text-center font-bold">Let's collaborate.</h1>
+      <p className="text-center mt-4 text-graytext">
+        I'm available for Consultancy & Freelancing.
+      </p>
       <div className="flex justify-center">
-        <Button
-          btnName="Say Hi"
-          addClass="bg-graytext"
-        />
-</div>
+        <Button btnName="Say Hello" addClass="bg-graytext" />
+      </div>
       <div className="flex justify-center gap-x-6 pt-[10%]">
-        <a href="https://github.com/jaykhans"> <i class="fa-brands fa-lg fa-github"></i> </a>
-        <a href="https://www.linkedin.com/in/jakhongirnagmatov/"> <i class="fa-brands fa-lg fa-linkedin"></i> </a>
-        <a href="https://twitter.com/jakhongir_mir"> <i class="fa-brands fa-lg fa-twitter"></i> </a>
-        <a href=""> <i class="fa-brands fa-lg fa-instagram"></i> </a>
-        <a href="https://t.me/jaykhans"> <i class="fa-brands fa-lg fa-telegram"></i> </a>
-
+        <a href="https://github.com/jaykhans">
+          <i class="fa-brands fa-lg fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/jakhongirnagmatov/">
+          <i class="fa-brands fa-lg fa-linkedin"></i>
+        </a>
+        <a href="https://twitter.com/jakhongir_mir">
+          <i class="fa-brands fa-lg fa-twitter"></i>
+        </a>
+        <a href="">
+          <i class="fa-brands fa-lg fa-instagram"></i>
+        </a>
+        <a href="https://t.me/jaykhans">
+          <i class="fa-brands fa-lg fa-telegram"></i>
+        </a>
       </div>
       <div className="text-center pt-6 pb-2">
-      <p className="text-sm">Made with ❤️ by Jakhongir Nagmatov.</p>
+        <p className="text-xs">Made with ❤️ by Jakhongir Nagmatov.</p>
       </div>
-
-        
-        
-      
     </div>
   )
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className="h-20 w-full fixed z-10 top-0 bg-transparent pt-5 px-16">
-        <nav className="flex justify-between items-end text-graytext">
-            <div className="">
-                <Link to="/" className="text-5xl text-black font-bold">JN</Link>
-            </div>
-                <div className="flex justify-end gap-10">
-                    <Link to="/about">About</Link>
-                    <Link to="/works">Works</Link>
-                    <Link to="/contact">Contact</Link>
-                </div>
-        </nav>
+      <nav className="flex justify-between items-end text-graytext">
+        <div className="">
+          <Link to="/" className="text-5xl text-black font-bold">
+            JN
+          </Link>
+        </div>
+        <div className="flex justify-end gap-10">
+          <Link to="/about">about</Link>
+          <Link to="/works">works</Link>
+          <Link to="/contact">contact</Link>
+        </div>
+      </nav>
     </div>
   )
 }
