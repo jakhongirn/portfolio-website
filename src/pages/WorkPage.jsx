0d7ce18de4ac28852d6bc4cbe0c3ac20e6ada384
jaykhans <jahongirnagmatov@gmail.com>
2022-08-29 11:28:18 +0500

    worksPage update

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -4,10 +4,11 @@ import { WorkImage, WorkContent } from '../components/cards/Work'
 const WorkPage = () => {
   return (
     <div className="bg-lightgray py-[15%] px-[15%]">
-        <div className="">
-            <h1 className="text-5xl font-bold">My works.</h1>
+        <div className="relative pb-[10%]">
+            <img src="images/works-pattern.jpg" className="w-[400px]" alt="" />
+            <h1 className="text-6xl font-bold bg-white absolute top-[40%]">My works.</h1>
         </div>
-        <div className="grid grid-cols-3 gap-y-20 mt-10">
+        <div className="grid grid-cols-3 gap-y-24 mt-10">
         <WorkImage imageName="meras.uz.png"/>
         <WorkContent 
             class="ml-28"
@@ -31,6 +32,13 @@ const WorkPage = () => {
             type="Web app"
             content="Website that offers medical articles and papers to educate people about HIV."
         />
+        <WorkContent
+            class="mr-28"
+            title="ecogosht - meat products and services"
+            type="Front-end App"
+            content="Business website that provides people excellent meat products and services."
+        />
+        <WorkImage imageName="ecogosht.png"/>
 
         
 
