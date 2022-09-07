b1e52ea8a0ac6154b746356b76b8cc085f877ea3
jaykhans <jahongirnagmatov@gmail.com>
2022-09-07 18:37:54 +0500

    add ContactPage and connect emailjs

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import emailjs from '@emailjs/browser'
+
+
+const ContactPage = () => {
+
+  const form = useRef();
+
+  const SERVICE_ID=process.env.REACT_APP_EMAIL_SERVICE_ID
+  const TEMPLATE_ID=process.env.REACT_APP_EMAIL_TEMPLATE_ID
+  const PUBLIC_KEY=process.env.REACT_APP_EMAIL_PUBLIC_KEY
+
+  const [status, setStatus] = useState(false);
+  
+  const sendEmail = (e) => {
+    
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+    .then((result) => {
+      console.log("Success!", result.text);
+      setStatus(true)
+      e.target.reset();
+    }, (error) => {
+      console.log('Failed', error.text);
+      setStatus(false)
+    })
+  }
+
+  useEffect(() => {
+    if(status) {
+      setTimeout(() => {
+        setStatus(false)
+      }, 3000)
+    }
+  }, [status])
+
+  return (
+    <div className="bg-lightgray py-[15%] px-[15%]">
+        <div className="uppercase tracking-[0.2rem] text-[10px] text-graytext rotate-[270deg] fixed top-64 left-10">works</div>
+        <div className="relative pb-[10%]">
+            <img src="images/contact-pattern.jpg" className="w-[400px]" alt="" />
+            <h1 className="text-6xl font-bold bg-white absolute top-[40%]">Get in touch.</h1>
+        </div>
+        <p>Do you have questions to ask or request to collaborate with me, feel free to contact me.</p>
+        <form action="" ref={form} onSubmit={sendEmail} >
+            <input className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" type="text" name="name" placeholder='Your Name'/>
+            <input className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" type="email" name="email" placeholder='Your mail'/>
+            <input className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" type="text" name="subject" placeholder='Subject'/>
+            <textarea className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" rows="5" name="message" placeholder='Message'/>
+          <button className="py-2 px-4 float-right bg-black border-2 border-black text-white hover:bg-white hover:text-black+">Send</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default ContactPage
