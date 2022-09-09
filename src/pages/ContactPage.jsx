1a5343416e70961bd87b69a4690842fae71774d3
jaykhans <jahongirnagmatov@gmail.com>
2022-09-09 12:40:36 +0500

    emailjs update in Contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import emailjs from '@emailjs/browser'
 
 
@@ -37,6 +37,11 @@ const ContactPage = () => {
 
   return (
     <div className="bg-lightgray py-[15%] px-[15%]">
+      {status ? 
+      <div className={`animate__animated animate__fadeInRight fixed z-50 bottom-16 right-4 px-8 py-4 bg-green-500 text-white font-semibold rounded-lg `}>
+        <p className='text-xl'>Successfully sent!</p>
+      </div>
+      : ''}
         <div className="uppercase tracking-[0.2rem] text-[10px] text-graytext rotate-[270deg] fixed top-64 left-10">works</div>
         <div className="relative pb-[10%]">
             <img src="images/contact-pattern.jpg" className="w-[400px]" alt="" />
@@ -48,7 +53,7 @@ const ContactPage = () => {
             <input className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" type="email" name="email" placeholder='Your mail'/>
             <input className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" type="text" name="subject" placeholder='Subject'/>
             <textarea className="block py-2 my-8 border-b-gray-200 border-b-2 w-full focus:outline-none" rows="5" name="message" placeholder='Message'/>
-          <button className="py-2 px-4 float-right bg-black border-2 border-black text-white hover:bg-white hover:text-black+">Send</button>
+          <button type="submit" className="py-2 px-4 float-right bg-black border-2 border-black text-white hover:bg-white hover:text-black">Send</button>
         </form>
 
     </div>
