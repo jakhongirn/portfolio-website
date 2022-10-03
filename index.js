3addf7155685332152b0e45231496f6d6fa97e83
jaykhans <jahongirnagmatov@gmail.com>
2022-10-03 11:46:55 +0500

    add animate when goTop appears

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 import App from './src/App.jsx'
 import './src/index.css'
 import "inter-ui/inter.css"
+import 'animate.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- src/components/cards/GoTop.jsx
+++ src/components/cards/GoTop.jsx
@@ -1,14 +1,16 @@
 import React, {useState, useEffect} from 'react'
-import { Fade } from 'react-awesome-reveal';
 
 const GoTop = () => {
     const [goTop, setGoTop] = useState(false)
+    const [animated, setAnimated] = useState("")
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
             if(window.scrollY > 710) {
                 setGoTop(true)
+                setAnimated("animate__animated animate__fadeInRightBig")
             } else {
+                setAnimated("animate__animated animate__fadeOutRightBig")
                 setGoTop(false)
             }            
         })
@@ -23,27 +25,27 @@ const GoTop = () => {
   return (
     <>  
         {goTop && (
-               
-                    <button onClick={scrollUp} className="rotate-90 text-graytext tracking-widest transition-all duration-300 hover:-translate-y-3 text-sm font-semibold  fixed right-4 bottom-20 uppercase z-10" ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="w-6 inline rotate-180"
-                              >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                    />
-                              </svg>
-                        Top
-                        </button>
-                
+                        <button onClick={scrollUp} className={`transition-all duration-300 translate-y-0 hover:-translate-y-3 ${animated} rotate-90 text-graytext tracking-widest text-sm font-semibold  fixed right-4 bottom-20 uppercase z-10`}><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 inline rotate-180"
+                                  >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                        />
+                                  </svg>
+                            Top
+                            </button>                
         )}
     </>
   )
 }
 
 export default GoTop
+
+//There is a bug with adding animation!
