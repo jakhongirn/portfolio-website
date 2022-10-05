c54c66640643a4c2c7a1061806f96240aae5950a
jaykhans <jahongirnagmatov@gmail.com>
2022-10-05 11:31:43 +0500

    migrate app to the create-react-app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
-import App from './src/App.jsx'
-import './src/index.css'
+import App from './App'
+import './index.css'
 import "inter-ui/inter.css"
 import 'animate.css'
 
