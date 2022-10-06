33a2c79527826165dde00010678e74dd4d16bf6c
jaykhans <jahongirnagmatov@gmail.com>
2022-10-06 17:30:59 +0500

    make hidden div title in mobile screen

--- src/components/cards/GhProject.jsx
+++ src/components/cards/GhProject.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GhProject = (props) => {
   return (
     <div className="flex gap-x-5 mt-10 items-center">
-      <p className="text-graytext mr-4 tracking-widest">{props.projectNumber}</p>
+      <p className="text-graytext mr-4 tracking-widest hidden md:block">{props.projectNumber}</p>
       <p className="underline">&lt;src/&gt; </p>
       <a className="text-2xl underline" href="#">
         {props.projectName}
