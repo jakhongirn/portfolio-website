c2b80ce293c5245f84bf421ea8754b1674e25a31
jaykhans <jahongirnagmatov@gmail.com>
2022-10-08 18:39:21 +0500

    formatting and add ScrollTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Layout from './layout/Layout'
-import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/HomePage/HomePage'
 import AboutPage from './pages/AboutPage'
-import WorkPage from './pages/WorkPage';
+import WorkPage from './pages/WorkPage'
 import ContactPage from './pages/ContactPage'
-import NoPage from './pages/404Page';
-import {AnimatePresence} from 'framer-motion'
+import NoPage from './pages/404Page'
+import { AnimatePresence } from 'framer-motion'
+import ScrollTop from './components/cards/ScrollTop'
 
 const App = () => {
   const location = useLocation()
   return (
     <>
-    <AnimatePresence mode="wait">
-      <Layout>
-        <Routes key={location.pathname} location={location}>
-          <Route exact path="/" element={<HomePage />}  />
-          <Route path="about" element={<AboutPage />}/>
-          <Route path="works" element={<WorkPage />}/>
-          <Route path="contact" element={<ContactPage />}/>
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </Layout>
+      <AnimatePresence mode="wait">
+        <ScrollTop>
+          <Layout>
+            <Routes key={location.pathname} location={location}>
+              <Route exact path="/" element={<HomePage />} />
+              <Route path="about" element={<AboutPage />} />
+              <Route path="works" element={<WorkPage />} />
+              <Route path="contact" element={<ContactPage />} />
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </Layout>
+        </ScrollTop>
       </AnimatePresence>
     </>
   )
--- src/components/cards/Work.jsx
+++ src/components/cards/Work.jsx
@@ -1,48 +1,31 @@
-import Button from './Button';
-import RevealAnimate from './RevealAnimate';
-import FadeUp from './FadeUp';
+import Button from './Button'
+import RevealAnimate from './RevealAnimate'
+import FadeUp from './FadeUp'
 
-import works from './dataWorks';
-
-export default function Work() {
-
-  const displayWorks = works.map((work, index) => {
-    function IsEven() {
-      if (index % 2 === 0) {
-        return true
-      } else {
-        return false
-      }
-    }
-    return (
-      <div className="grid grid-cols-3 my-24">
-        <div className={`col-span-2 ${IsEven() ? 'mr-28' : ' order-last ml-28'}`}>
-          <RevealAnimate>
-            <h1 className="mt-4 text-5xl font-bold">{work.title}</h1>
-          </RevealAnimate>
-          <FadeUp>
-            <p className="uppercase mt-4 text-sm tracking-widest">{work.type}</p>
-          </FadeUp>
-          <FadeUp>
-            <p className="mt-10 font-medium text-lg">
-              {work.content}
-            </p>
-          </FadeUp>
-            <Button
-            btnName="View project"
-            ></Button>
-        </div>
-        <div className={` ${IsEven() ? '' : ''}`}>
-        <RevealAnimate><img src={`images/${work.imageName}`} className="rounded-xl" alt="" /></RevealAnimate>
-      </div>
-      </div>
-      )
-  })
-  
-
+export default function Work(props) {
   return (
-    <>
-      {displayWorks}
-    </>
+    <div className="grid grid-cols-1 lg:grid-cols-3 py-12 gap-y-12">
+      <div>
+        <RevealAnimate>
+          <img
+            src={`images/${props.imageName}`}
+            className="rounded-xl"
+            alt=""
+          />
+        </RevealAnimate>
+      </div>
+      <div className={props.classContent}>
+        <RevealAnimate>
+          <h1 className="mt-4 text-5xl font-bold">{props.title}</h1>
+        </RevealAnimate>
+        <FadeUp>
+          <p className="uppercase mt-4 text-sm tracking-widest">{props.type}</p>
+        </FadeUp>
+        <FadeUp>
+          <p className="mt-10 font-medium text-lg">{props.content}</p>
+        </FadeUp>
+        <Button btnName="View project"></Button>
+      </div>
+    </div>
   )
 }
